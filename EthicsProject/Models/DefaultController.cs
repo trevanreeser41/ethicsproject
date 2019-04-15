a9a33428a370623c2df500b40142dc6ee57c1060
Ethan Guinn <et.guinn@gmail.com>
2019-04-15 17:49:25 -0600

    Actually updated nuget packages

--- EthicsProject/Models/DefaultController.cs
+++ EthicsProject/Models/DefaultController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace EthicsProject.Models
