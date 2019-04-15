fbba68a8a638ebd9f3651d6dd9c7ddeb75e7bc18
Ethan Guinn <et.guinn@gmail.com>
2019-04-15 17:09:45 -0600

    Add project files.

---
+++ EthicsProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EthicsProject.Startup))]
+namespace EthicsProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
